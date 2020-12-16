0c86bcb758e59372e3d3693d193745710f996eb0
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-12-16 09:23:17 +0300

    Add project files.

---
+++ ageebSoft.CountryName/MyCountry.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ageebSoft.CountryName
+{
+    public class MyCountry
+    {
+        public int Id { get; set; }
+        public string NameEn { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Capital_En { get; set; }
+
+
+
+        public static List<MyCountry> getAllCountry()
+        {
+
+            var folderPath = Environment.CurrentDirectory;
+            var FilePath = Path.Combine(folderPath, "Country.json");
+            var fileText = File.ReadAllText(FilePath);
+            return JsonConvert.DeserializeObject<List<MyCountry>>(fileText);
+
+
+
+        }
+    }
+
+    
+
+     
+}
---
+++ ageebSoft.CountryName/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ageebSoft.CountryName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in MyCountry.getAllCountry())
+            {
+                Console.WriteLine("Name : "+item.Name +" , EngName : "+item.NameEn);
+            } ;
+
+            Console.ReadLine();
+        }
+
+
+
+
+        
+
+    }
+}
